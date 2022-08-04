503073a4ea7cd57d980dae300bdf9f43897cad2d
NikitaMuhin <mukhin2000070700@gmail.com>
2022-08-04 09:21:47 +0300

    Initial commit

---
+++ src/main/java/com/example/creditscoring/CreditScoringCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoringCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoringCalculationApplication.class, args);
+	}
+
+}
